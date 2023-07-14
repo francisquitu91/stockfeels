c19e481d88360753a1bd5ce91bef5acd3246b073
Harrison <koharrisonko@gmail.com>
2023-07-14 00:03:16 -0500

    added streamlit and made model deployable

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
+import streamlit as st
 
 def sentimentAnalysis(tickers):
     finviz_url = 'https://finviz.com/quote.ashx?t='
@@ -62,16 +63,41 @@ def sentimentAnalysis(tickers):
     #takes date column and converts string to date time format (easy for matplot lib to recognize)
     df['date'] = pd.to_datetime(df.date).dt.date
 
-    # plt.figure(figsize=(20,16))
+    plt.figure(figsize=(20,16))
 
-    # #mean function only looks for integer values in a row and averages em up, group by ticker, date isolates those columns
-    # mean_df = df.groupby(['ticker', 'date']).mean()
-    # #returns datafram with new level of column labels who's inner most level consists of pivoted index labels
-    # mean_df = mean_df.unstack()
-    # #cross section
-    # mean_df = mean_df.xs('compound', axis="columns").transpose()
-    # mean_df.plot(kind='bar')
-    # plt.show()
+    #mean function only looks for integer values in a row and averages em up, group by ticker, date isolates those columns
+    mean_df = df.groupby(['ticker', 'date']).mean()
+    #returns datafram with new level of column labels who's inner most level consists of pivoted index labels
+    mean_df = mean_df.unstack()
+    #cross section
+    mean_df = mean_df.xs('compound', axis="columns").transpose()
+    mean_df.plot(kind='bar')
+    st.pyplot(plt)
     return df
-with open('stockSentiment.pkl', 'wb') as file:
-    pickle.dump(sentimentAnalysis, file)
+
+def show_page():
+    st.title("Stock Sentiment Analysis")
+    st.write("Using the news, this website predicts if a stock will go up or down at given times using machine learning and sentiment analysis!")
+    st.write("""### Which stocks do you want to see today?""")
+
+    stocks1 = (
+        'TSLA',
+        'AAPL',
+        'AMZN',
+        'GOOGL',
+        'COST',
+        'MSFT',
+        'NVDA' 
+    )
+    stocksChosen = []
+
+    for i in stocks1:
+        chosen = st.checkbox(i)
+        if(chosen):
+            stocksChosen.append(i)
+
+    ok = st.button("See the stats!")
+    if ok:
+        sentimentAnalysis(stocksChosen)
+
+show_page()
